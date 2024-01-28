6b3f778044f1539fd839dd2af3f9f9da4daa1bce
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-01-28 16:17:51 +0530

    structuring & testing ints

---
+++ integers/adder_test.go
@@ -0,0 +1,34 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func assertCorrectMessage(t testing.TB, got, want int) {
+	// ? t.Helper() is needed to tell the test suite that this method is a helper
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestAdder(t *testing.T) {
+	t.Run("test 1", func(t *testing.T) {
+		got := Add(2, 2)
+		want := 4
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("test 2", func(t *testing.T) {
+		got := Add(-2, 2)
+		want := 0
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+}
