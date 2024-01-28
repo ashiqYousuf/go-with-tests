d9caa865f1237648812e0a1eb499374fccd2d65f
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-01-28 15:39:19 +0530

    Added tests for our Hello World

--- hello.go
+++ hello.go
@@ -2,15 +2,37 @@ package main
 
 import "fmt"
 
-const HELLO = "Hello, "
+const (
+	EN_PREFIX = "Hello, "
+	SP_PREFIX = "Hola, "
+	FR_PREFIX = "Bonjour, "
 
-func Hello(name string) string {
-	// if name == "" {
-	// 	name = "World"
-	// }
-	return fmt.Sprintf("%s%s", HELLO, name)
+	SPANISH = "Spanish"
+	FRENCH  = "French"
+)
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case FRENCH:
+		prefix = FR_PREFIX
+	case SPANISH:
+		prefix = SP_PREFIX
+	default:
+		prefix = EN_PREFIX
+	}
+
+	return
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	prefix := greetingPrefix(language)
+	return fmt.Sprintf("%s%s", prefix, name)
 }
 
 func main() {
-	fmt.Println(Hello("Brain"))
+	fmt.Println(Hello("Brain", ""))
 }
